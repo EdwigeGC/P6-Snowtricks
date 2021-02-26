e938b659f42c32b7fedb845652b62c4a95bdd199
Edwige Genty <edwige.gentymail@gmail.com>
2021-02-26 16:49:54 +0100

    feature to add Picture/video in edit trick form

--- P6_Snowtricks/src/Entity/Video.php
+++ P6_Snowtricks/src/Entity/Video.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\VideoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=VideoRepository::class)
@@ -23,6 +24,11 @@ class Video
     private $title;
 
     /**
+     * @Assert\Regex(
+     *     pattern="#^https:\/\/[a-z0-9._-]+\.[a-z]{2,4}/embed/#",
+     *     match=true,
+     *     message="Your url must contain /embed"
+     * )
      * @ORM\Column(type="string", length=255)
      */
     private $url;
