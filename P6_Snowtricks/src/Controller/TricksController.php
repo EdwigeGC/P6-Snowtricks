3096fc1131123733f7d175c0b498dc16cb8b476d
Edwige Genty <edwige.gentymail@gmail.com>
2021-03-03 16:23:58 +0100

    correction param voters

--- P6_Snowtricks/src/Controller/TricksController.php
+++ P6_Snowtricks/src/Controller/TricksController.php
@@ -199,7 +199,7 @@ class TricksController extends AbstractController
     public function edit(Trick $trick, Request $request, ObjectManager $manager): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-        if($this->isGranted('edit', $trick)){
+        if($this->isGranted('POST_EDIT', $trick)){
             $form=$this->createForm(EditTrickType::class, $trick);
             $form->handleRequest($request);
 
